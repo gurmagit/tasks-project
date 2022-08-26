afe28aa7c44ef81f6e02e358d49be4267bc53c7c
gurmagit <gurma@netvision.net.il>
2022-08-26 09:22:03 +0300

    clean code

---
+++ client/src/app/tasks/task-create.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IsLoggedInService } from '../../services/is-logged-in.service';
+import { TasksService } from '../../services/tasks.service';
+import { UsersService } from '../../services/users.service';
+import { User } from '../task.model';
+
+@Component({
+  selector: 'app-task-create',
+  templateUrl: './task-create.component.html',
+  styleUrls: ['./task-create.component.css']
+})
+export class TaskCreateComponent implements OnInit{
+  constructor(
+    public tasksService: TasksService,
+    public usersService: UsersService,
+    public isLoggedInService: IsLoggedInService
+  ) {}
+  private user = {} as User;
+  isUserLoggedIn = this.isLoggedInService.isAuthenticated;
+
+  ngOnInit(): void {
+    this.user = this.usersService.getUser();
+    console.log('token:', this.isLoggedInService.getToken());
+  }
+
+  onCreateTask(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const data = {
+      title: form.value.title,
+      content: form.value.content,
+      author: this.user.username,
+      date: new Date()
+    }
+    this.tasksService.createTask(data);
+    form.resetForm();
+  }
+}
