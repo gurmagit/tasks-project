ef0a2f8079b5c5562291bfead702b072434805d1
gurmagit <gurma@netvision.net.il>
2022-08-30 02:00:07 +0300

    Add files via upload

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatDialogModule } from '@angular/material/dialog';
 import { DatePipe } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +17,7 @@ import { TaskListComponent } from './tasks/task-list/task-list.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { EditTaskComponent } from './tasks/edit-task/edit-task.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
